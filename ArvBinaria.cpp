61c5370b4f5c541c0eaae1e41bcab11233200809
Kelvin Modesto <km.modesto@gmail.com>
2014-05-09 19:32:11 -0300

    Atualização

--- ArvBinaria.cpp
+++ ArvBinaria.cpp
@@ -3,10 +3,30 @@
 
  class ArvBinaria{
  private:
-
+    int tamanho;
  public:
+    ArvBinaria(int tam);
+    int getTamanho(){
+        return tamanho;
+    }
+    void setTamanho(int tam){
+        tamanho=tam;
+    }
+    void inicializar(int t){
+        setTamanho(t);
+    }
+    void inserir(){
+    }
+    void remover(){
+    }
+    void buscar(){
+    }
 
  };
+//Construtor da classe
+ ArvBinaria::ArvBinaria(int tam){
+    inicializar(tam);
+ }
  int main(){
      cout << "Sempre que vc fizer alguma coisa poste nesse repositorio https://github.com/ProjetoArvBinaria-EDII"<< endl;
      return 0;
